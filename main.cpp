ed648a6d9869a5dfbc3eb46b5c5e28b093de4e22
icaromarley5 <icaromarley5@gmail.com>
2016-04-19 16:38:47 -0300

    primeiro processador de video

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+//imports das bibliotecas
+
+
+int main (int argc, char** argv){
+
+//receber um vídeo no formato AVI como entrada
+
+/*
+
+ o seu programa deve aplicar um conjunto de métodos de
+processamento de imagens em cada frame do vídeo. 
+A aplicação de cada método deve obrigatoriamente modificar a imagem e ter relevância no processo. O conjunto
+de métodos aplicados deve resultar em um efeito coerente no vídeo final. 
+
+4 métodos diferentes devem ser utilizados
+
+
+
+aplicar, no mínimo:
+- Três operadores pontuais ou aritméticos;
+- Cinco filtros espaciais ou morfológicos.
+
+
+*/
+
+//retornar um vídeo processado (também no formato AVI) como saída.
+
+}
