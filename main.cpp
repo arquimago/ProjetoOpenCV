5fe12609626d72a05c5bde57fce4ea0d4f9af3fd
icaromarley5 <icaromarley5@gmail.com>
2016-04-22 01:38:08 -0300

    adicionei mais includes e mais filtros, organizei comentários

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 using namespace cv;
+using namespace std;
+
 
 int main (int argc, char** argv){
     //inicializações
@@ -7,24 +11,23 @@ int main (int argc, char** argv){
     IplImage* frame; //frame do video
     //adquirir propriedades do video
     int isColor = 1;
-    int fps = ( int )cvGetCaptureProperty( capture, CV_CAP_PROP_FPS );
-    int frameH    = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
-    int frameW    = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-    int nFrames = (int) cvGetCaptureProperty(capture,  CV_CAP_PROP_FRAME_COUNT);
+    int fps = (int)cvGetCaptureProperty( capture, CV_CAP_PROP_FPS );
+    int frameH  = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+    int frameW  = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+    int nFrames = (int)cvGetCaptureProperty(capture,  CV_CAP_PROP_FRAME_COUNT);
+    int widthStep = frame->widthStep;
+    int channels  = frame->nChannels;
     //retornar um vídeo processado (também no formato AVI) como saída.
     CvVideoWriter *writer = cvCreateVideoWriter("out.avi",CV_FOURCC('P','I','M','1'),
                            fps,cvSize(frameW,frameH),isColor);
 
     // processamento
-    int key = 0;
     IplImage* img;
     for(int i=0;i<nFrames - 1;i++){
         cvGrabFrame(capture);          // captura imagem
         img=cvRetrieveFrame(capture);  // recupera a imagem capturada
         cvShowImage("Video Original", img); //mostra imagem original
         /*
-
-
         o seu programa deve aplicar um conjunto de métodos de
         processamento de imagens em cada frame do vídeo.
         A aplicação de cada método deve obrigatoriamente modificar a imagem e ter relevância no processo. O conjunto
@@ -32,17 +35,10 @@ int main (int argc, char** argv){
 
         4 métodos diferentes devem ser utilizados
 
-
-
         aplicar, no mínimo:
         - Três operadores pontuais ou aritméticos;
         - Cinco filtros espaciais ou morfológicos.
 
-
-
-
-        #########################IDEIAS PARA O PROGRAMA#########################
-        Algumas sequencias de operações desejaveis:
 
 
         processo com 4 ou mais filtros: esconder informação e extrair informação
@@ -50,42 +46,28 @@ int main (int argc, char** argv){
         (outro para extrair essa informação pelo processo inverso)
         (caso seja usado outro processo para tentar extrair informação, a imagem não deve ser entendível)
 
-
-        usar operações únicas e reversíveis
-        (inversão de cor, escurecimento, realçar picos)
-        sequencias de operações reversíveis
-        (inversão de cores->escurecimento)
-        (realçar picos -> inversão de cores)
-
+        alguns filtros
+        http://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
         http://docs.opencv.org/2.4/modules/imgproc/doc/filtering.html
 
-        alguns filtros
-        //SUAVIZAÇÃO cvSmooth(out, out, CV_GAUSSIAN, 9);
-
-
-        ###########################################################################
-
-
+        cvSobel(img, img, 1, 1, 3 );
+        cvSmooth(img, img, CV_GAUSSIAN, 9);
+        cvLaplace(img, img, 3 );
+        cvDilate(img,img, NULL, 5 );
+        cvErode(img,img, NULL, 5 );
+        cvNot(img, img);
+        cvCvtColor(img, img, CV_BGR2Luv);
+        cvThreshold(img,img, 30, 700, 1);
         */
-        //alguns filtros
-        //cvSobel(img, img, 1, 1, 3 );
-        //cvSmooth(img, img, CV_GAUSSIAN, 9);
-        //cvLaplace(img, img, 3 );
-        //cvDilate(img,img, NULL, 5 );
-        //cvErode(img,img, NULL, 5 );
-        //cvShowImage("Video modificado", img); //mostra imagem modificada
-
+        cvShowImage("Video modificado", img); //mostra imagem modificada
 
         cvWriteFrame(writer,img);      // grava imagem no video de saída
         cvWaitKey(1);           // espera 1ms
     }
 
-
-
-    //release the capture source
+    //liberar recursos
     cvReleaseCapture(&capture);
     cvReleaseVideoWriter(&writer);
-
     return 0;
 
 }
